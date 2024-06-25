2c224b23c17d9cb7056c88a817c0ce21707b0236
Amirhossein Abolhasani <46609807+AmirAAZ818@users.noreply.github.com>
2024-06-25 13:15:57 +0330

    some changes

--- Chapter7_Fixed_Partitioning/System run log.py
+++ Chapter7_Fixed_Partitioning/System run log.py
@@ -1,7 +1,7 @@
 from MultiQ_sys import Multi_Queue_Sys
 from SingleQ_sys import Single_Queue_Sys
 
-m_Sys_RR = Single_Queue_Sys(memory_size=1024, p_amount=5,
+m_Sys_RR = Single_Queue_Sys(memory_size=1024, p_amount=20,
                             show_log=True, scheduler='SRT')
 
 m_Sys_RR.run_sys()
